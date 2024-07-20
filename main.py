6692356ec209341a1ac3d423388b3fb3289df110
scilla <sr.cilla@gmail.com>
2024-07-20 17:30:16 +0200

    chore: remove unused import

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import smtplib
 import csv, os
-from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
